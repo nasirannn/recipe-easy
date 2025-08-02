3b9e15ccd63cca769cbb2de2ad51506b2656908b
Annnb <annnb016@gmail.com>
2025-08-02 22:42:13 +0800

    优化introduce字体展示

--- components/layout/sections/introduce.tsx
+++ components/layout/sections/introduce.tsx
@@ -15,7 +15,7 @@ export const IntroduceSection = () => {
           <h2 className="text-3xl md:text-4xl font-bold mb-4">
             {t("heading")}
           </h2>
-          <p className="text-xl text-muted-foreground mb-8">
+          <p className="text-lg text-muted-foreground mb-8">
             {t("description1")}
           </p>
           <p className="text-lg text-muted-foreground mb-8">
